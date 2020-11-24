0b3692bb5baaca65609b4070386909b3faacfe01
morgmaestro <70662172+morgmaestro@users.noreply.github.com>
2020-11-23 19:56:14 -0800

    week 11 colors!

--- week11/canvas.js
+++ week11/canvas.js
@@ -6,9 +6,18 @@ let cPosY = canvas.height/2;
 let cVelX = 3.4;
 let cVelY = 4.2;
 let cRad = 60;
-let cColor = "yellow";
+
+let cRed = document.getElementById("cRed");
+let cOrange = document.getElementById("cOrange");
+let cYellow = document.getElementById("cYellow");
+let cGreen = document.getElementById("cGreen");
+let cBlue = document.getElementById("cBlue");
+let cIndigo = document.getElementById("cIndigo");
+let cViolet = document.getElementById("cViolet");
 
 let clicked = false;
+
+context.fillStyle = "yellow";
 
 function draw(){
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -21,7 +30,7 @@ function draw(){
 
 function drawCircle() {
     context.beginPath();
-    context.fillStyle = cColor;
+    
     context.globalAlpha = 0.7;
     context.ellipse(cPosX, cPosY, cRad, cRad, 0, 0, 2*Math.PI);
     context.fill();
@@ -54,8 +63,80 @@ canvas.addEventListener("click", function(event){
 
     if(distX < cRad && distY < cRad){
         console.log(clicked);
+        counter += 1;
         clicked = !clicked;
     }
 });
+
+function drawRed() {
+    context.fillStyle = "rgba(255, 0, 0, .7)";
+    context.fill();
+    window.requestAnimationFrame(drawRed);
+}
+
+cRed.addEventListener("click", function(event) {
+    drawRed();
+});
+
+function drawOrange() {
+    context.fillStyle = "rgba(255, 165, 0, .7)";
+    context.fill();
+    window.requestAnimationFrame(drawOrange);
+}
+
+cOrange.addEventListener("click", function(event) {
+    drawOrange();
+});
+
+function drawYellow() {
+    context.fillStyle = "rgba(255, 255, 0, .7)";
+    context.fill();
+    window.requestAnimationFrame(drawYellow);
+}
+
+cYellow.addEventListener("click", function(event) {
+    drawYellow();
+});
+
+function drawGreen() {
+    context.fillStyle = "rgba(0, 128, 0, 0.7)";
+    context.fill();
+    window.requestAnimationFrame(drawGreen);
+}
+
+cGreen.addEventListener("click", function(event) {
+    drawGreen();
+});
+
+function drawBlue() {
+    context.fillStyle = "rgba(0, 0, 255, .7)";
+    context.fill();
+    window.requestAnimationFrame(drawBlue);
+}
+
+cBlue.addEventListener("click", function(event) {
+    drawBlue();
+});
+
+function drawIndigo() {
+    context.fillStyle = "rgba(75, 0, 130, .7)";
+    context.fill();
+    window.requestAnimationFrame(drawIndigo);
+}
+
+cIndigo.addEventListener("click", function(event) {
+    drawIndigo();
+});
+
+function drawViolet() {
+    context.fillStyle = "rgb(238, 130, 238, .7)";
+    context.fill();
+    window.requestAnimationFrame(drawViolet);
+}
+
+cViolet.addEventListener("click", function(event) {
+    drawViolet();
+});
+
 
 // offsetX & offsetY give the point of a click
