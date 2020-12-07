ed6e9befaaf63d8856dc63e31ab965e893e26388
morgmaestro <70662172+morgmaestro@users.noreply.github.com>
2020-12-06 17:00:31 -0800

    sunday done

--- week13/birdbox/script.js
+++ week13/birdbox/script.js
@@ -1,18 +1,17 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     $('.carousel').carousel({
-        interval : 5000,
+        interval: 5000,
         keyboard: false,
     });
-    
+
     let carouselPaused = false;
 
-    $('.js-carousel-pause').click(function() {
-        if(carouselPaused) {
+    $('.js-carousel-pause').click(function () {
+        if (carouselPaused) {
             $('.carousel').carousel('cycle');
             $(this).html('<i class="fas fa-pause"></i>');
-        }
-        else {
+        } else {
             $('.carousel').carousel('pause');
             $(this).html('<i class="fas fa-play"></i>');
         }
@@ -23,19 +22,43 @@ $(document).ready(function() {
     $("#signUp").modal('show');
 
     let form = document.getElementById('myForm');
-    form.addEventListener("submit", function(evt){
-      if(form.checkValidity() === false) {
-        evt.preventDefault();
-        evt.stopPropagation();
-      }
+    form.addEventListener("submit", function (evt) {
+        if (form.checkValidity() === false) {
+            evt.preventDefault();
+            evt.stopPropagation();
+        }
 
-      form.classList.add('was-validated');
+        form.classList.add('was-validated');
     })
-
-
 
 });
 
+console.log($(this).prop('checked'));
+$('#billingCheck').click(function () {
+    if ($(this).prop('checked')) {
+        $('#shippingAddress1').val($('#billingAddress1').val());
+        $('#shippingAddress2').val($('#billingAddress2').val());
+        $('#shippingCity').val($('#billingCity').val());
+        $('#shippingState').val($('#billingState').val());
+        $('#shippingZip').val($('#billingZip').val());
+    } else {
+        $('#shippingAddress1').val('');
+        $('#shippingAddress2').val('');
+        $('#shippingCity').val('');
+        $('#shippingState').val('');
+        $('#shippingZip').val('');
+    }
+})
+
+let form = document.getElementById('myForm');
+form.addEventListener("submit", function (evt) {
+    if (form.checkValidity() === false) {
+        evt.preventDefault();
+        evt.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+});
 
 
 // javascript is an event based language
