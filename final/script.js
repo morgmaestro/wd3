d6aa877df6ace5665b5a0c4d848d8d99d1872447
morgmaestro <70662172+morgmaestro@users.noreply.github.com>
2020-12-15 14:27:30 -0800

    fix validate

--- final/script.js
+++ final/script.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
     // modal
     $("#signUp").modal('show');
 
+    //add to cart
     $(".addCart").click(function() {
         let q = parseInt($(this).prev(".quantity").val());
         let currentQ = parseInt($("#cartCount").text());
@@ -32,31 +33,4 @@ $(document).ready(function () {
         $("#cartCount").text(q);
     });
 
-    let form = document.getElementById('modalForm');
-    form.addEventListener("submit", function (evt) {
-        if (form.checkValidity() === false) {
-            evt.preventDefault();
-            evt.stopPropagation();
-        }
-
-        form.classList.add('was-validated');
-    });
-
-});
-
-$('#billingCheck').click(function () {
-    console.log($(this).prop('checked'));
-    if ($(this).prop('checked')) {
-        $('#shippingAddress1').val($('#billingAddress1').val());
-        $('#shippingAddress2').val($('#billingAddress2').val());
-        $('#shippingCity').val($('#billingCity').val());
-        $('#shippingState').val($('#billingState').val());
-        $('#shippingZip').val($('#billingZip').val());
-    } else {
-        $('#shippingAddress1').val('');
-        $('#shippingAddress2').val('');
-        $('#shippingCity').val('');
-        $('#shippingState').val('');
-        $('#shippingZip').val('');
-    }
 });
